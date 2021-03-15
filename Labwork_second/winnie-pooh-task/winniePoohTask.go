14c25ca69949fda1e414ad5da3b9ef86fd1afa47
Vlad Yakymenko <vladyslav.yakymenko@intapp.com>
2021-03-15 11:58:01 +0200

    LAB-2: Small refactoring

--- Labwork_second/winnie-pooh-task/winniePoohTask.go
+++ Labwork_second/winnie-pooh-task/winniePoohTask.go
@@ -7,17 +7,17 @@ import (
 	"time"
 )
 
-func setUpMatrix(n, m int) [][]bool {
-	matrix := make([][]bool, n)
+func createForest(rows, columns int) [][]bool {
+	matrix := make([][]bool, rows)
 	for i := range matrix {
-		matrix[i] = make([]bool, m)
+		matrix[i] = make([]bool, columns)
 	}
 
 	// set the Bear position
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	var (
-		x = r.Intn(n)
-		y = r.Intn(m)
+		x = r.Intn(rows)
+		y = r.Intn(columns)
 	)
 
 	matrix[x][y] = true
@@ -37,15 +37,16 @@ func regionToString(region []bool) string {
 	return str
 }
 
-func findBear(id int, waitGroup *sync.WaitGroup, regions <-chan []bool, bearFoundSignal chan bool) {
+func findBear(id int, waitGroup *sync.WaitGroup, regions <-chan []bool, bearFound chan bool) {
 	defer waitGroup.Done()
 
 	for region := range regions {
 		select {
-		case <-bearFoundSignal:
-			bearFoundSignal <- true
+		case <-bearFound:
+			bearFound <- true
 			fmt.Println()
 			return
+
 		default:
 			str := regionToString(region)
 			fmt.Println("Bees group", id, "searching the bear in a region ", str)
@@ -54,38 +55,36 @@ func findBear(id int, waitGroup *sync.WaitGroup, regions <-chan []bool, bearFoun
 				time.Sleep(time.Microsecond * 100)
 				if i {
 					fmt.Println("Bees group", id, "found the bear in a region", str, " on position", index)
-					bearFoundSignal <- true
+					bearFound <- true
 					fmt.Println("Bees group", id, "returned.")
 					return
 				}
 			}
 		}
+
 		fmt.Println("Bees group", id, "returned.")
 	}
 }
 
 func main() {
 	const (
-		//n is number of rows
-		n = 10
-		//n is number of rows
-		m = 100
-		//beesNum is number of bees
-		beesNum = 4
+		rows            = 10
+		columns         = 100
+		beeGroupsAmount = 4
 	)
-	forestMatrix := setUpMatrix(n, m)
-	regions := make(chan []bool, n)
-	bearFoundSignal := make(chan bool, 1)
+	forest := createForest(rows, columns)
+	regions := make(chan []bool, rows)
+	bearFound := make(chan bool, 1)
 
 	var waitGroup sync.WaitGroup
 
-	for i := 0; i < beesNum; i++ {
+	for i := 0; i < beeGroupsAmount; i++ {
 		waitGroup.Add(1)
-		go findBear(i, &waitGroup, regions, bearFoundSignal)
+		go findBear(i, &waitGroup, regions, bearFound)
 	}
 
-	for i := 0; i < n; i++ {
-		regions <- forestMatrix[i]
+	for i := 0; i < rows; i++ {
+		regions <- forest[i]
 	}
 
 	close(regions)
